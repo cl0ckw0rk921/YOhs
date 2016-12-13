605fe86853e3ea4436a02d079b5ef16dbc362f2c
cl0ckw0rk42 <danielboggs@Daniels-MacBook-Air.local>
2016-12-12 21:09:27 -1000

    MoreEmail
    updated email target

--- controllers/contact.js
+++ controllers/contact.js
@@ -35,7 +35,7 @@ exports.postContact = (req, res) => {
   }
 
   const mailOptions = {
-    to: 'your@email.com',
+    to: 'dgboggs001@email.com',
     from: `${req.body.name} <${req.body.email}>`,
     subject: 'Contact Form | Hackathon Starter',
     text: req.body.message
